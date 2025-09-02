74f72d639e654d464e662eadbe9167a8e05af7e9
Eric Larson <ewlarson@gmail.com>
2025-09-02 11:00:38 -0500

    Tests: working and green again

--- db/database.py
+++ db/database.py
@@ -9,9 +9,5 @@ load_dotenv()
 # Get the database URL from the environment variable
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Convert the DATABASE_URL to use asyncpg if it's using postgres://
-if DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
-
 # Create the database instance
 database = Database(DATABASE_URL)
