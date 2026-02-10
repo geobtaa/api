2e8d72f1dc728206d53178ffa69a8e9656452772
Eric Larson <ewlarson@gmail.com>
2026-02-09 18:55:07 -0600

    refactor: Remove deprecated featured resource IDs from configuration
    - Eliminated unused resource IDs from the `FEATURED_RESOURCE_IDS` array in `featured.ts`, streamlining the configuration for featured resources.

--- frontend/src/config/featured.ts
+++ frontend/src/config/featured.ts
@@ -3,8 +3,6 @@
  * IDs correspond to /resources/{id} paths.
  */
 export const FEATURED_RESOURCE_IDS: string[] = [
-  'a10a0f50-994e-0134-2096-0050569601ca-c', // Map
-  '5MHz_6km_realtime-agg', // Dataset
   'c69b75a12ca64324a0109d48db735f6d_0', // WebServices
   'eee6150b-ce2f-4837-9d17-ce72a0c1c26f', // Imagery
   '11c34a17-bc6d-429c-8770-64888ccb5302', // Allmaps georeferenced map (resource_allmaps)
